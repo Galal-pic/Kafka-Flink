05e17cd1191ec67ef9b13455de8c28f2a6ace750
Galal Ewida <galalamrewida@gmail.com>
2024-12-08 09:07:59 +0200

    change partition method

--- src/data_generator.py
+++ src/data_generator.py
@@ -14,14 +14,9 @@ def set_orders(producer, topic_name, data_generator):
     try:
         for i, fraud_data in enumerate(data_generator):
             is_fraud = fraud_data["is-Fraud"]  # Determine fraud status
-            partition = 0 if is_fraud == "No" else 1
 
-            producer.send(
-                topic_name, key=str(is_fraud), value=fraud_data, partition=partition
-            )
-            print(
-                f"Produced record {i} to topic '{topic_name}' in partition {partition} with key '{is_fraud}'."
-            )
+            producer.send(topic_name, key=str(is_fraud), value=fraud_data)
+            print(f"Produced record {i} to topic '{topic_name}' with key '{is_fraud}'.")
             time.sleep(4)
     except Exception as e:
         logging.error(f"Error sending messages to Kafka: {e}")
