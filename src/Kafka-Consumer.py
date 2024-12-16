edaae9cc27747defffc0a787d9b048414a9114a9
Galal Ewida <galalamrewida@gmail.com>
2024-12-17 01:40:52 +0200

    ReorganizeKafka

--- src/Kafka-Consumer.py
+++ src/Kafka-Consumer.py
@@ -1,26 +1,30 @@
-from config.Kafka_config import create_kafka_consumer
+from config.kafka import create_kafka_consumer
+
+
 def consume_messages(topic_name, group_id=None):
     consumer = create_kafka_consumer(topic_name)
-    
     if consumer is None:
         return
-    
     try:
         print(f"Starting to consume messages from topic: {topic_name}")
         for message in consumer:
             try:
                 # Print detailed message information
-                print(f"Topic: {message.topic} Partition: {message.partition} Offset: {message.offset}")
+                print(
+                    f"Topic: {message.topic} Partition: {message.partition} Offset: {message.offset}"
+                )
                 print(f"Key: {message.key}")
                 print(f"Value: {message.value}")
                 print("-" * 50)
             except Exception as e:
                 print(f"Error processing message: {e}")
-    
+
     except KeyboardInterrupt:
         print("Stopping consumer...")
     finally:
         consumer.close()
+
+
 if __name__ == "__main__":
-    topic_name= 'New-Topic'
+    topic_name = "topic1"
     consume_messages(topic_name)
